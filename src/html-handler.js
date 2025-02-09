019fb7bb616d8751694a4838e4a957a31091a3bd
tot121212 <totmusicbusiness@gmail.com>
2025-02-09 15:51:25 -0600

    btns now fadeinout

--- src/html-handler.js
+++ src/html-handler.js
@@ -10,16 +10,27 @@ export class HTMLHandler {
 
     static init = ()=>{
         this.updateObjectWithSVG("object.three-bars-svg", threeBarsSVG);
+
         const nav = document.querySelector("nav");
         const settingsBtn = nav.querySelector("button#settings");
-        const settingsBtns = nav.querySelector(".btns");
+        const btns = nav.querySelector(".btns");
+
         settingsBtn.addEventListener("click", ()=>{
-            settingsBtns.hidden ? settingsBtns.hidden = false : settingsBtns.hidden = true;
+            btns.hidden ? btns.hidden = false : btns.hidden = true;
 
             settingsBtn.classList.add("fade");
             setTimeout(() => {
                 settingsBtn.classList.remove("fade");
-            }, 200);
+            }, 300);
         });
+
+        for (const btn of btns.querySelectorAll("button")){
+            btn.addEventListener("click", ()=>{
+                btn.classList.add("fade");
+                setTimeout(() => {
+                    btn.classList.remove("fade");
+                }, 300);
+            });
+        }
     }
 }
